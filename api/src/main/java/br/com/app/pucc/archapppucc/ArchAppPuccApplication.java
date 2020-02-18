9f484b7481aaeb2d8c066d0c7bf2994c0cc73e23
Matheus Pedroni <matheusr@ciandt.com>
2020-02-18 19:27:16 -0300

    [Sprint-0] Environment setup

---
+++ api/src/main/java/br/com/app/pucc/archapppucc/ArchAppPuccApplication.java
@@ -0,0 +1,13 @@
+package br.com.app.pucc.archapppucc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchAppPuccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchAppPuccApplication.class, args);
+	}
+
+}
