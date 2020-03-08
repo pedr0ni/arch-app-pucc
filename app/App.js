c8a2ae814d11a3a3cb1867907700aa7a90e5453c
Matheus Pedroni <pedroni.dev@gmail.com>
2020-03-07 21:20:32 -0300

    [Sprint-X] App login page

--- app/App.js
+++ app/App.js
@@ -1,19 +1,71 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+import { Ionicons } from '@expo/vector-icons';
+
+import { Colors } from './utils/DefaultStyles';
+
+import LoginScreen from './views/LoginScreen';
+import HomeScreen from './views/HomeScreen';
+import ProfileScreen from './views/ProfileScreen';
+
+const loginNavigation = createStackNavigator({
+	Login: {
+		screen: LoginScreen,
+		navigationOptions: {
+			header: null
+		}
+	}
 });
+
+const defaultNavigation = createBottomTabNavigator({
+	Home: {
+		screen: HomeScreen,
+		navigationOptions: {
+			tabBarIcon: ({ tintColor }) => (
+				<Ionicons name={Platform.OS === 'ios' ? "ios-home" : 'md-home'} size={24} color={tintColor} />
+			),
+			tabBarLabel: 'Grade'
+		}
+	},
+	Profile: {
+		screen: ProfileScreen,
+		navigationOptions: {
+			tabBarIcon: ({ tintColor }) => (
+				<Ionicons name={Platform.OS === 'ios' ? "ios-home" : 'md-home'} size={24} color={tintColor} />
+			),
+			tabBarLabel: 'Perfil'
+		}
+	}
+}, {
+	tabBarOptions: {
+		activeTintColor: Colors.defaultBlue,
+		labelStyle: {
+			fontSize: 12
+		}
+	}
+})
+
+const mainNavigation = createStackNavigator({
+	Default: {
+		screen: defaultNavigation,
+		navigationOptions: {
+			header: null
+		}
+	},
+	Login: {
+		screen: loginNavigation,
+		navigationOptions: {
+			header: null
+		}
+	}
+}, {
+	initialRouteName: 'Login'
+});
+
+console.disableYellowBox = true;
+
+export default createAppContainer(mainNavigation);
---
+++ app/utils/DefaultStyles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const Colors = {
+    defaultBlue: '#16336c'
+};
+
+export const Styles = StyleSheet.create({
+    defaultShadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5
+    }
+});
